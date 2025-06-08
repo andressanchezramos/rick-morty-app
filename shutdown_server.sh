d9e3156613432c388bdee2f145b1e95069513936
Andres Sanchez <andres.sanchez@mitigasolutions.com>
2025-06-08 09:13:58 +0200

    update docs

--- shutdown_server.sh
+++ shutdown_server.sh
@@ -2,8 +2,3 @@
 
 # Stop and remove Docker containers
 docker compose down
-
-# Deactivate the virtual environment if active
-if [[ "$VIRTUAL_ENV" != "" ]]; then
-    deactivate
-fi
--- start_server.sh
+++ start_server.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Activate the virtual environment
-source .venv/bin/activate
-
-# Install the required packages
-pip install -r requirements.txt
-
 # Start Redis and PostgreSQL using Docker Compose
 docker-compose up -d
-
-# Run the FastAPI server
-uvicorn api.api:app --reload
